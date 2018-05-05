561768b4c65438bd444ebca3bf7ebf7c65a0d808
Brian O'Reilly <fade@deepsky.com>
2018-05-05 14:10:20 -0400

    Initial commit.
    This is the first iteration of a system to pull wallpapers from the
    wallhaven.cc website. At the moment, it only pulls out of the random
    category, which means that the wallpapers don't include any NSFW
    content. The obvious TODO would be to permit a user to provide
    credentials, and have the app log in to the site, and then provide
    category tags for returned images.

---
+++ decorator.asd
@@ -0,0 +1,20 @@
+;; -*-lisp-*-
+;;;; decorator.asd
+
+(asdf:defsystem #:decorator
+  :description "Download some high resolution wallpapers blindly. A graphical chocolate box, really."
+  :author "Brian O'Reilly <fade@deepsky.com>"
+  :license "Lisp Lesser General Public License."
+  :serial t
+  :depends-on (:cl-ppcre
+               :alexandria
+               :rutils
+               ;; :drakma
+               ;; :lparallel
+               :trivial-download
+               :simple-date-time
+               :cl-mechanize)
+  :pathname "./"
+  :components ((:file "app-utils")
+               (:file "decorator")))
+
---
+++ decorator.lisp
@@ -0,0 +1,63 @@
+;; -*-lisp-*-
+(defpackage :decorator
+  (:use #:cl
+        #:decorator.app-utils
+        #:simple-date-time
+        #:trivial-download
+        ;; #:lparallel
+        ;; #:drakma
+        #:cl-ppcre
+        #:cl-mechanize)
+  (:export :-main))
+
+(in-package :decorator)
+
+(defparameter *picture-storage*
+  (ensure-directories-exist 
+   (merge-pathnames
+    (format nil "Desktop_pics/~A/"
+            (simple-date-time:YYYYMMDD (simple-date-time:from-universal-time (get-universal-time))))
+    (user-homedir-pathname)))
+  "datestamped destination directory for the images we are downloading.")
+
+(defparameter *full-wallpaper-path* "https://wallpapers.wallhaven.cc/wallpapers/full/")
+
+(defvar *browser* (make-instance 'browser)
+  "this creates a 'browser' object which is used by the rest of the mechanize protocol.")
+
+(defun get-links (&key (url "https://alpha.wallhaven.cc/random"))
+  "returns a list of URLs pointing to random wallpapers at wallhaven.cc."
+  (fetch url *browser*)
+  (let ((results (browser-page *browser*))
+        (linkresults nil))
+    (dolist (link (page-links results))
+      (if (cl-ppcre:scan "^.*\/wallpaper/\\d+$" (link-url link))
+          (push (link-url link) linkresults)))
+    linkresults))
+;; (stp:do-recursively (node results)
+;;       (format t "~A~%" node))
+;; (dolist (link (page-links results))
+;;         (format t "~A~%" (link-uri link)))
+
+(defun getit (link &key (url "https://wallpapers.wallhaven.cc/wallpapers/full"))
+  (let* ((seqnumber (first (last (rutils:split-sequence #\/ link))))
+         (outname (format nil "~A~A~A" "wallhaven-" seqnumber ".jpg"))
+         (outpath (merge-pathnames outname *picture-storage*))
+         (fileurl (format nil "~A/~A" url outname)))
+    (format t "~&~{~A~^~%~}" (list seqnumber outname outpath link fileurl))
+    (let ((no nil))
+      (handler-case (download fileurl outpath)
+        (error (c)
+          (format t "~&Caught condition, ~A" c)
+          (push c no)
+          nil)))))
+
+(defun doit (linklist)
+  "Given a list of target URL, from wallhaven, get the full sized
+wallpaper represented by each one."
+  (loop for link in linklist
+        for thing = (getit link)
+        :collect thing))
+
+(defun -main (&optional args)
+  (doit (get-links)))
